7fc1edc7ebdd80e1dea11e0ced094a1715c6c67f
abdi <igdabdipramana@gmail.com>
2023-04-13 18:26:15 +0800

    add filter by property type

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,8 +7,12 @@ import * as style from './Home.scss.json';
 import Spinner from '../components/Spinner';
 import { HousePreview } from "../components/HousePreview";
 
+const FILTER_DATA = ['All', 'House', 'Villa', 'Apartment'];
+
 export default function Home() {
-  let [houses, setHouses] = useState<ShapeSet<House>>()
+  let [houses, setHouses] = useState<ShapeSet<House>>();
+  const [filter, setFilter] = useState("All");
+
   useEffect(() => {
     Storage.promiseUpdated().then(() => {
       let houses = House.getLocalInstances();
@@ -16,21 +20,46 @@ export default function Home() {
     })
   }, [])
 
+  const handleFilterClick = (selectedFilter: string) => {
+    setFilter(selectedFilter);
+  };
+
+  const filteredHouses = houses?.filter(({ propertyType: type }) => {
+    if (filter === 'All') {
+      return true;
+    } else {
+      return type === filter;
+    }
+  });
+
   return (
     <div className={style.Home}>
       {/* <h2>Code challenge</h2>
       <div className={style.housesContainer}>{houses && <p>{houses?.size} houses are ready to be used</p>}</div> */}
       <div className={style.container}>
         <div className={style.headlineSection}>
-          <span className={style.categories}>Our Recommendation</span>
-          <h3 className={style.headline}>Featured House</h3>
+          <div>
+            <span className={style.categories}>Our Recommendation</span>
+            <h3 className={style.headline}>Featured House</h3>
+          </div>
+          <div className={style.filter}>
+            {FILTER_DATA.map((option) => (
+              <button 
+                key={option} 
+                className={option === filter ? style.activeFilterButton : ''}
+                onClick={() => handleFilterClick(option)}
+              >
+                {option}
+              </button>
+            ))}
+          </div>
         </div>
         {!houses && (
           <Spinner />
         )}
         <div className={style.grid}>
-          {houses && houses.map((house: House) => (
-            <HousePreview of={house} />
+          {filteredHouses?.map((house: House) => (
+            <HousePreview of={house} key={house.namedNode.uri} />
           ))}
         </div>
       </div>
