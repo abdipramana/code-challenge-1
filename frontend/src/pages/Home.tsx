4829b12449c6f9d6db38020f6d52c82b017d4c9b
abdi <igdabdipramana@gmail.com>
2023-04-13 18:03:28 +0800

    create house card

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import { House } from '../shapes/House';
 import './Home.scss';
 import * as style from './Home.scss.json';
+import Spinner from '../components/Spinner';
+import { HousePreview } from "../components/HousePreview";
 
 export default function Home() {
   let [houses, setHouses] = useState<ShapeSet<House>>()
@@ -13,10 +15,25 @@ export default function Home() {
       setHouses(houses);
     })
   }, [])
+
   return (
     <div className={style.Home}>
-      <h2>Code challenge</h2>
-      <div className={style.housesContainer}>{houses && <p>{houses?.size} houses are ready to be used</p>}</div>
+      {/* <h2>Code challenge</h2>
+      <div className={style.housesContainer}>{houses && <p>{houses?.size} houses are ready to be used</p>}</div> */}
+      <div className={style.container}>
+        <div className={style.headlineSection}>
+          <span className={style.categories}>Our Recommendation</span>
+          <h3 className={style.headline}>Featured House</h3>
+        </div>
+        {!houses && (
+          <Spinner />
+        )}
+        <div className={style.grid}>
+          {houses && houses.map((house: House) => (
+            <HousePreview of={house} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
